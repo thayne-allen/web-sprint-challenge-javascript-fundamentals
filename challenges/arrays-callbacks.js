3aba1f9ddd21f71d9b79336f91b5e808e4f8e481
thayne-allen <thayne.allen@gmail.com>
2020-06-12 19:44:33 -0600

    finished .map challenge

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -32,8 +32,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
-console.log(lowCaseAnimalNames);
+const lowCaseAnimalNames = zooAnimals.map((zooAnimal, index, zooAnimals) => {
+  return zooAnimal.animal_name.toLowerCase();
+});
 
 /* Request 3: .filter() 
 
